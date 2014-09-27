693b2fbbb032201b5aeff7de0e48f601f77960d9
Jef King <jef@agilebusinesscloud.com>
2014-09-27 10:36:29 -0700

    updated

---
+++ King.Data.Sql.Reflection.Data/dbo/Sprocs/ManyTypes.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE [dbo].[ManyTypes]
+	@TestInt int = NULL
+	, @TestBigInt bigint = NULL
+	, @TestBit bit = NULL
+	, @TestDecimal decimal(16,16) = NULL
+	, @TestMoney money = NULL
+	, @TestFloat float = NULL
+	, @TestDate date = NULL
+	, @TestDateTime2 datetime2 = NULL
+	, @TestDateTime datetime = NULL
+	, @TestChar char = NULL
+	, @TestText text = NULL
+	, @TestNChar nchar = NULL
+	, @TestNText ntext = NULL
+	, @TestBinary binary(64) = NULL
+	, @TestImage image = NULL
+	, @TestGuid uniqueidentifier = NULL
+AS
+BEGIN
+
+	SELECT @TestInt AS 'Identifier'
+		, @TestBigInt AS 'BigInt'
+		, @TestBit AS 'Bit'
+		, @TestDecimal AS 'Decimal'
+		, @TestMoney AS 'Money'
+		, @TestFloat AS 'Float'
+		, @TestDate AS 'Date'
+		, @TestDateTime2 AS 'DateTime2'
+		, @TestDateTime AS 'DateTime'
+		, @TestChar AS 'Char'
+		, @TestText AS 'Text'
+		, @TestNChar AS 'NChar'
+		, @TestNText AS 'NText'
+		, @TestBinary AS 'Binary'
+		, @TestImage AS 'Image'
+		, @TestGuid AS 'Unique'
+
+END
