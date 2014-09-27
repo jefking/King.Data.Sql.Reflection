25a98ecf125df9de87581fd9ebcaea29ad493dda
Jef King <jef@agilebusinesscloud.com>
2014-09-27 09:35:24 -0700

    Test Projects

---
+++ King.Data.Sql.Reflection.Integration.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("King.Data.Sql.Reflection.Integration.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("King.Data.Sql.Reflection.Integration.Test")]
+[assembly: AssemblyCopyright("Copyright © Jef King 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("59691fc5-35f8-4a78-b3e7-4a8afbb98f0f")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ King.Data.Sql.Reflection.Unit.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("King.Data.Sql.Reflection.Unit.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("King.Data.Sql.Reflection.Unit.Test")]
+[assembly: AssemblyCopyright("Copyright © Jef King 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("21267ccc-cf2a-4418-b80e-74e110e6d771")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
