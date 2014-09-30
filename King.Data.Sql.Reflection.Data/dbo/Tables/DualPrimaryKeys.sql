df9a199392b0abf5d61a50fdf517cff39d408b6b
Jef King <jef@agilebusinesscloud.com>
2014-09-30 12:05:05 -0700

    dual primary keys

---
+++ King.Data.Sql.Reflection.Data/dbo/Tables/DualPrimaryKeys.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[DualPrimaryKeys]
+(
+	[FirstId] INT NOT NULL , 
+    [SecondId] INT NOT NULL, 
+    [SomeData] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(), 
+    PRIMARY KEY ([SecondId], [FirstId])
+)
