fb7ba59cdbd011bed8a3de422d03a651f8c55a9b
Jef King <jef@agilebusinesscloud.com>
2015-03-16 15:39:05 -0700

    Boom

---
+++ King.Data.Sql.Reflection.46/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("King.Data.Sql.Reflection")]
+[assembly: AssemblyDescription("SQL Server Schema Relfection.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("King.Data.Sql.Reflection")]
+[assembly: AssemblyCopyright("Copyright © Jef King 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("aebbbff7-178f-4f9e-b192-434b8a5512df")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
